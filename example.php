f0c1e17fc59c6cdbdf38eb9ae8e11965510ce29c
Lige <30907749+iSinser@users.noreply.github.com>
2018-10-02 02:08:35 +0800

    example

--- example.php
+++ example.php
@@ -1,26 +1,24 @@
 <?php 
-require 'vendor/autoload.php'
-//require '/vendor/autoload.php';若使用源码方式下载SDK 请将本条引用的注释去除，将上一条引用注释
+require('sinser-php-sdk/src/main.php'); 
 
 $sinser = new sinser;
-$user = '';//ID 即登录帐号 需改动
-$ak = '';//SID 需改动
-$sk = '';//SecretKey 需改动
+$user = 'admin';//ID 即登录帐号 需改动
+$ak = 'XUNXI71AtUrh4XpL0fuQ';//SID 需改动
+$sk = 'oNwVPlMyJBcFwSxlLxIWjDMe';//SecretKey 需改动
 $salt = rand('100000','999999');//盐值 无需改动
 $sign_time = time();//签名时间戳 无需改动
 $Authorization = $sinser -> getAuthorization($user,$ak,$sk,$sign_time,$salt);//生成签名
 
 $ret = $sinser -> statistics($ak,$Authorization);//发送统计请求
 print_r ($ret);
-//单次签名有效期为20s 超时需重新生成
+//单次签名有效期为30s 超时需重新生成
 
 
-//getdata 参数说明
-//第三个参数=json时 返回json格式数据
-//第三个参数为1-13时 返回对应单个数据
-$ret = $sinser -> getdata($ak,$sk,'json');//获取统计数据
+$ret = $sinser -> getdata(base64_encode($ak),sha1($sk));//获取统计数据
 print_r ($ret);
 
+//$ret = $sinser -> online($ak,$Authorization);//用户上线（已集成在统计接口中，该独立接口仅用于保持用户心跳，以达到用户长期在线，自行选择是否使用）
+//print_r ($ret);
 
 ?>
 
